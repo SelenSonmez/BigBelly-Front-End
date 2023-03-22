c28a1d14412b93ca7037288d983cdd7f439f503c
Selen Sönmez <selensonmez2001@gmail.com>
2023-03-22 21:26:03 +0300

    tile update

--- lib/screens/search/widgets/profile_tile.dart
+++ lib/screens/search/widgets/profile_tile.dart
@@ -6,36 +6,71 @@ import '../../../constants/styles.dart';
 
 class ProfileTile extends StatelessWidget {
   const ProfileTile(
-      {super.key, required this.username, required this.followerCount});
+      {super.key,
+      required this.username,
+      required this.followerCount,
+      this.isRequest = false});
+
   final String username;
   final int followerCount;
+  final bool isRequest;
+
   @override
   Widget build(BuildContext context) {
-    return Card(
-      shadowColor: Colors.green,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(15.0),
-      ),
-      elevation: 10,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          ListTile(
-            leading: CircleAvatar(
-              backgroundColor: mainThemeColor,
-              radius: 22.h,
-              child: CircleAvatar(
-                backgroundImage: AssetImage(defaultProfileImage),
-                radius: 20.0.h,
-              ),
-            ),
-            title: Text(
-              username,
-              style: const TextStyle(fontWeight: FontWeight.bold),
-            ),
-            subtitle: Text("$followerCount Followers"),
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(10.0.h),
+        child: Card(
+          shadowColor: Colors.green.shade500,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15.0.w),
           ),
-        ],
+          elevation: 7,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              ListTile(
+                leading: CircleAvatar(
+                  backgroundColor: mainThemeColor,
+                  radius: 22.h,
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage(defaultProfileImage),
+                    radius: 20.0.h,
+                  ),
+                ),
+                title: Text(
+                  username,
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                ),
+                subtitle: Text("$followerCount Followers"),
+                trailing: SizedBox(
+                  width: 100.w,
+                  height: 75.h,
+                  child: isRequest
+                      ? Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            IconButton(
+                              icon: const Icon(
+                                Icons.add,
+                                color: Colors.green,
+                              ),
+                              onPressed: () {},
+                            ),
+                            IconButton(
+                                onPressed: () {},
+                                icon: const Icon(
+                                  Icons.close,
+                                  color: Colors.red,
+                                ))
+                          ],
+                        )
+                      : null,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
