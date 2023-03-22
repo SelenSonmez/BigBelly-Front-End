164630b85b4ee5a1ebfe39a7e933052c944b50b7
Selen Sönmez <selensonmez2001@gmail.com>
2023-03-22 16:18:08 +0300

    create post init, combo box

--- lib/screens/add_post/add_post_screen.dart
+++ lib/screens/add_post/add_post_screen.dart
@@ -1,16 +1,43 @@
+import 'package:bigbelly/screens/authentication/helpers/big_belly_text_field.dart';
+import 'package:dropdown_button2/dropdown_button2.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:number_inc_dec/number_inc_dec.dart';
 
 import '../imports.dart';
+import 'widgets/big_belly_dropdown.dart';
 
 class AddPostScreen extends StatelessWidget {
-  const AddPostScreen({super.key});
+  AddPostScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
-      child: Text("addpost"),
+      child: Row(
+        children: [IncrementButton(), BigBellyDropdown()],
+      ),
     ));
+  }
+}
+
+class IncrementButton extends StatelessWidget {
+  const IncrementButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+            width: 60,
+            height: 45,
+            margin: const EdgeInsets.all(15.0),
+            // padding: const EdgeInsets.all(,.0),
+            child: TextField(
+              keyboardType: TextInputType.number,
+            )),
+      ],
+    );
   }
 }
