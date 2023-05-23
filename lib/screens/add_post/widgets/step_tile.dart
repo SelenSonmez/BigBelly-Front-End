ea10bd097fffb51763716242fa554d3f7b13768b
Selen Sönmez <selensonmez2001@gmail.com>
2023-05-23 16:45:34 +0300

    fromJson functions added

--- lib/screens/add_post/widgets/step_tile.dart
+++ lib/screens/add_post/widgets/step_tile.dart
@@ -4,15 +4,17 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../../constants/providers/post_provider.dart';
 
 class StepTile extends ConsumerStatefulWidget {
-  StepTile({super.key, required this.step, required this.stepIndex});
+  StepTile({super.key, required this.step, this.stepIndex});
 
   String step;
-  int stepIndex;
+  int? stepIndex;
 
   @override
   ConsumerState<StepTile> createState() => _StepTileState();
 
-  static fromMap(Map<String, dynamic> x) {}
+  factory StepTile.fromMap(Map<String, dynamic> map) {
+    return StepTile(step: map['description']);
+  }
 
   Map<String, dynamic> toMap() {
     return <String, dynamic>{'description': step};
@@ -65,7 +67,7 @@ class _StepTileState extends ConsumerState<StepTile> {
                     // post.getPost.steps![widget.stepIndex].step = newValue;
                     // post.getPost.steps![widget.stepIndex].stepIndex =
                     //     widget.stepIndex;
-                    post.getPost.steps![widget.stepIndex - 1].step = newValue;
+                    post.getPost.steps![widget.stepIndex! - 1].step = newValue;
                     // post.getPost.steps!.add(StepTile(
                     //   step: newValue,
                     //   stepIndex: widget.stepIndex,
